d7ee62ec34a5066b9018e2df948f977457250312
chatial <chatialfc@gmail.com>
2022-10-26 23:30:53 +0700

    Menyelesaikan Vidio 3 & 4

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Ahmad Reyhan Ronaldo",
+        "email" => "chatialfc@gmail.com",
+        "image" => "ahmad.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+
